320da8c04de6b8a271452266e973c73841257534
WenConrad <conradwen@gmail.com>
2021-10-15 09:56:33 -0700

    manipulating objects

---
+++ concat-array.js
@@ -0,0 +1,15 @@
+function concat(array1, array2) {
+  let concatenated = [];
+  for (let element of array1) {
+    concatenated.push(element);
+  }
+  for (let element of array2) {
+    concatenated.push(element);
+  }
+  return concatenated
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
