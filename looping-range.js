943a9f54f7660b5212bfee7d4d7e00eee452b4ab
WenConrad <conradwen@gmail.com>
2021-10-15 03:53:12 -0700

    changed if statement

--- looping-range.js
+++ looping-range.js
@@ -11,6 +11,3 @@ function range(start, end, step) {
 console.log(range(0, 10, 2));
 console.log(range(10, 30, 5));
 console.log(range(-5, 2, 3));
-
-console.log(range(2, 0, -1));
-console.log(range(0, 10, 0));
